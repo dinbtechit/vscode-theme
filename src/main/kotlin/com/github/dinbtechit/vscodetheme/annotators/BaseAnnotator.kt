c5cd95f931ba2f05e3519f42fac1664298902095
Dinesh <dinbtechit@gmail.com>
2023-03-12 22:57:00 -0300

    chores: Syncing up with the template changes.

--- src/main/kotlin/com/github/dinbtechit/vscodetheme/annotators/BaseAnnotator.kt
+++ src/main/kotlin/com/github/dinbtechit/vscodetheme/annotators/BaseAnnotator.kt
@@ -4,13 +4,14 @@ import com.intellij.lang.annotation.AnnotationHolder
 import com.intellij.lang.annotation.Annotator
 import com.intellij.lang.annotation.HighlightSeverity
 import com.intellij.openapi.editor.colors.TextAttributesKey
+import com.intellij.openapi.project.DumbAware
 import com.intellij.openapi.util.TextRange
 import com.intellij.psi.PsiComment
 import com.intellij.psi.PsiElement
 import com.intellij.psi.impl.source.tree.LeafPsiElement
 
 
-abstract class BaseAnnotator : Annotator {
+abstract class BaseAnnotator : Annotator, DumbAware {
 
     override fun annotate(element: PsiElement, holder: AnnotationHolder) {
 
